eb310efe622acf385734e6d455b4f08bcf61b029
Yusuf Enes Kuş <yusufenes.kus15@gmail.com>
2026-05-28 19:23:48 +0300

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -13,6 +13,6 @@ RESET_LOG_PATH = os.path.join(BASE_DIR,"logs", "reset.log")
 SENDEMAIL_LOG_PATH = os.path.join(BASE_DIR,"logs", "sendEMail.log")
 
 BOOK_CATEGORIES = ["Roman","Hikaye","Şiir","Biyografi","Otobiyografi","Tarih","Bilim","Kişisel Gelişim","Ders Kitabı","Ansiklopedi","Çizgi Roman"]
-GMAIL = "your_gmail"
+GMAIL = "yusufenes.kus15@gmail.com"
 
 CONFIG_JSON_PATH_FOR_SENDEMAIL = os.path.join(BASE_DIR,"db", "config.json")
