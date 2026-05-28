9002c5c2e9e5e5c07032c3e97398eb46248eff02
Yusuf Enes Kuş <yusufenes.kus15@gmail.com>
2026-05-28 19:24:58 +0300

    Update setupSQL.sql

--- backend/setupSQL.sql
+++ backend/setupSQL.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE IF NOT EXISTS library CHARACTER SET utf8mb4 COLLATE utf8mb4_turkish_ci;
 USE library;
 
-CREATE USER 'yonetici'@'127.0.0.1' IDENTIFIED BY 'Kutuphane@Yonetim#2026!';
+CREATE USER 'admin'@'127.0.0.1' IDENTIFIED BY 'Kutuphane@Yonetim#2026!';
 GRANT ALL PRIVILEGES ON library.* TO 'admin'@'127.0.0.1';
 
-CREATE USER 'yonetici'@'10.156.231.0' IDENTIFIED BY 'Kutuphane@Yonetim#2026!';
+CREATE USER 'admin'@'10.156.231.0' IDENTIFIED BY 'Kutuphane@Yonetim#2026!';
 GRANT ALL PRIVILEGES ON library.* TO 'admin'@'10.156.231.0';
 
 FLUSH PRIVILEGES;
@@ -41,7 +41,7 @@ whoAdded VARCHAR(20) NOT NULL);
 CREATE TABLE importantValues(id INT AUTO_INCREMENT PRIMARY KEY,
 situationValue TEXT NOT NULL, valueStatus BOOLEAN NOT NULL);
 
-INSERT INTO users (userName,userPassword,userRole) VALUES ('admin admin','your_password','admin');
+INSERT INTO users (userName,userPassword,userRole) VALUES ('admin admin','$2b$12$neXdHi87e40YGWtgeNEIYuqYsZbgwOHma7jehRWYhcgMbFq/h03qy','admin');
 INSERT INTO importantValues (situationValue,valueStatus) VALUES ('App Is Locked',FALSE);
 
 
